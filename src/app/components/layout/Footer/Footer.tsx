353c6417a4804635334c54d3383a832e7b543431
id0ntc4re <parsikovevgenij470@gmail.com>
2026-04-01 10:27:39 +0700

    qwe

--- src/app/components/layout/Footer/Footer.tsx
+++ src/app/components/layout/Footer/Footer.tsx
@@ -1,73 +1,83 @@
-import styles from "./Footer.module.scss"
+"use client";
+import styles from "./Footer.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Footer(){
-    return(
-        <section className={styles.footer}>
-            <div className={styles.footerContent}>
-                <div className={styles.arrows}>
-                    <Image src="/images/arrows.png" height={200} width={300} alt="arrows"></Image>
-                </div>
-                <div className={styles.navGrid}>
-                    <div className={styles.leftSide}>
-                        <Image src="/images/logo.png" height={200} width={300} alt="arrows"/>
-                        <div>
-                            <p>ООО &quot;КХМ&quot;</p>
-                            <p>Кемерово, ул. Какая-то, 1</p>
-                        </div>
-                        
-
-                    </div>
-                    <div className={styles.navSide}>
-                        <Link href="/#about">Компания</Link>
-                        <Link href="/#products">Продукция</Link>
-                        <Link href="/#services">Услуги</Link>
-                        <Link href="/#contacts">Контакты</Link>
-                    </div>
-                    <div className={styles.contactsSide}>
-                        <ul>
-                            <li>
-                                <a className={styles.footerContacts} href="ssd">
-                                    <Image 
-                                    src="/svg/mapPoint.svg"
-                                    alt="Адрес"
-                                    width={25}
-                                    height={25}
-                                    />
-                                    Адрес
-                                </a>
-                            </li>
-                            <li>
-                                <a className={styles.footerContacts} href={`tel:sdsd`}>
-                                    <Image 
-                                    src="/svg/phone.svg"
-                                    alt="Адрес"
-                                    width={25}
-                                    height={25}
-                                    />
-                                    Телефон
-                                </a>
-                            </li>
-                            <li>
-                                <a className={styles.footerContacts} href={`mailto:sdsds`}>
-                                    <Image 
-                                    src="/svg/mail.svg"
-                                    alt="Адрес"
-                                    width={25}
-                                    height={25}
-                                    />
-                                    email
-                                </a>
-                            </li>
-
-                        </ul>
-
-                    </div>
-                </div>
-
-
+export default function Footer() {
+  return (
+    <section className={styles.footer}>
+      <div className={styles.footerContent}>
+        <div className={styles.arrows}>
+          <Image
+            src="/images/arrows.png"
+            height={200}
+            width={300}
+            alt="arrows"
+          />
+        </div>
+        <div className={styles.navGrid}>
+          <div className={styles.leftSide}>
+            <Image src="/images/logo.png" height={200} width={300} alt="logo" />
+            <div>
+              <p>ООО &quot;КХМ&quot;</p>
+              <p>г. Кемерово, ул. Тухачевского, 54</p>
             </div>
-        </section>
-    );
+          </div>
+
+          <div className={styles.navSide}>
+            <Link href="/#about">Компания</Link>
+            <Link href="/#products">Продукция</Link>
+            <Link href="/#services">Услуги</Link>
+            <Link href="/#contacts">Контакты</Link>
+          </div>
+
+          <div className={styles.contactsSide}>
+            <ul>
+              <li>
+                <a
+                  className={styles.footerContacts}
+                  href="https://yandex.ru/maps/?text=Кемерово, ул. Тухачевского, 54"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    src="/svg/mapPoint.svg"
+                    alt="Адрес"
+                    width={25}
+                    height={25}
+                  />
+                  г. Кемерово, ул. Тухачевского, 54
+                </a>
+              </li>
+              <li>
+                <a className={styles.footerContacts} href="tel:89059650390">
+                  <Image
+                    src="/svg/phone.svg"
+                    alt="Телефон"
+                    width={25}
+                    height={25}
+                  />
+                  8 (905) 965-03-90
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.footerContacts}
+                  href="mailto:KHM-Group@mail.ru"
+                >
+                  <Image
+                    src="/svg/mail.svg"
+                    alt="Email"
+                    width={25}
+                    height={25}
+                  />
+                  KHM-Group@mail.ru
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/app/components/mainPage/Contacts/Contacts.tsx
+++ src/app/components/mainPage/Contacts/Contacts.tsx
@@ -1,53 +1,36 @@
-"use client"
-import styles from "./Contacts.module.scss"
+"use client";
+import styles from "./Contacts.module.scss";
 
+export default function Contacts() {
+  return (
+    <section className="container" id="contacts">
+      <div className={styles.contactsGrid}>
+        <div className={styles.help}>
+          <h2>Свяжитесь с нами</h2>
+          <div className={styles.line} />
+          <p>
+            Мы всегда готовы ответить на ваши вопросы и обсудить детали
+            сотрудничества
+          </p>
+        </div>
 
-export default function Contacts(){
-    return(
-        <section className="container" id="contacts">
-            
-            <div className={styles.contactsGrid}>
-                <div className={styles.help}>
-                    <h2>Нужна помощь с подбором?</h2>
-                    <div className={styles.line}/>
-                    <p>Оставьте заявку и наш специалист свяжется с вами в течение 15 минут</p>
-                </div>
-                <div className={styles.form}>
-                    <form action="">
-                        <div className={styles.formGroup}>
-                            <input 
-                                type="text" 
-                                id="name" 
-                                name="name" 
-                                placeholder="Введите ваше имя" 
-                                required 
-                            />
-                        </div>
-                        
-                        <div className={styles.formGroup}>
-                            <input 
-                                type="tel" 
-                                id="phone" 
-                                name="phone" 
-                                placeholder="+7 (___) ___-__-__" 
-                                required 
-                            />
-                        </div>
-                        
-                        <div className={styles.formGroup}>
-                            <textarea 
-                                id="comment" 
-                                name="comment" 
-                                placeholder="Ваш комментарий или вопрос" 
-                            ></textarea>
-                        </div>
-                        
-                        <button type="submit" className={styles.submitBtn}>
-                            <p>Оставить заявку</p>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </section>
-    );
+        <div className={styles.infoBlock}>
+          <div className={styles.contactItem}>
+            <span>Адрес:</span>
+            <p>г. Кемерово, ул. Тухачевского, 54</p>
+          </div>
+
+          <div className={styles.contactItem}>
+            <span>Телефон:</span>
+            <a href="tel:89059650390">8 (905) 965-03-90</a>
+          </div>
+
+          <div className={styles.contactItem}>
+            <span>E-mail:</span>
+            <a href="mailto:KHM-Group@mail.ru">KHM-Group@mail.ru</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
